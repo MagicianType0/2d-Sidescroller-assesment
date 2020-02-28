cf670fab058b14a5a5067560d9ce67d35ad34c63
Oce_Galaxy <magciantype0@gmail.com>
2020-02-28 11:40:54 +1100

    Add files via upload

---
+++ Generate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200f;
+    [SerializeField] private Transform LevelPart_Start;
+    [SerializeField] private List<Transform> LevelPartList;
+    [SerializeField] private PlayerMovement player;
+
+    private Vector3 lastEndPosition;
+
+    private void Awake()
+    {
+        lastEndPosition = LevelPart_Start.Find("EndPosition").position;
+        int startingSpawnLevelParts = 5;
+        for(int i = 0; i < startingSpawnLevelParts; i++)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform chosenLevelPart = LevelPartList[Random.Range(0, LevelPartList.Count)];
+        Transform lastLevelPartTransform = SpawnLevelPart(chosenLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+
+    }
+
+    private Transform SpawnLevelPart(Transform levelPart, Vector3 spawnPosition)
+    {
+        Transform levellPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return levellPartTransform;
+    }
+}
---
+++ PauseGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public GameObject pause;
+    public AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pause.activeInHierarchy)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pause.SetActive(true);
+        music.Pause();
+        
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        pause.SetActive(false);
+        music.Play();
+     
+    }
+    
+}
