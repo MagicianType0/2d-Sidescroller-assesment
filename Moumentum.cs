dfcd270fc06255d81abb464b7804b38f61341c26
Oce_Galaxy <magciantype0@gmail.com>
2020-02-28 12:15:44 +1100

    Add files via upload

---
+++ Moumentum.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moumentum : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb2D.AddForce(transform.right * speed);
+    }
+}
